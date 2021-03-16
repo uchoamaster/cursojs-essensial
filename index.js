eb7e13338592eb63e1cd6ad63094258931477afc
Carlos UchOa <carlosjuchoa@hotmail.com>
2021-03-16 11:35:34 -0300

    aprendendo como acessar elemento por id  ou pela class

---
+++ index.js
@@ -0,0 +1,16 @@
+//documento, procure um elemento com ID "meuTexto" e altere no HTML para o texto "Mudei o Texto!"
+ // var txt = document.getElementById("meuTexto").innerHTML;
+//console.log(txt);
+
+////////////////////////////////////////////////////////
+
+//formas de pegar elemento
+//pegar elemento pela tag
+// var txtTagName = document.getElementsByTagName("h1");
+// console.log(txtTagName[0].innerHTML);
+
+//quero pegar todos os elementos que estão utilizando uma classe
+//pegar elemento pelo nome da classe
+//document.getElementsByClassName();
+var txtClassName = document.getElementsByClassName("txtName");
+console.log(txtClassName);
