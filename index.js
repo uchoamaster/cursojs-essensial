f8d60e60fdc91fca8d9e85cca6633db67a94dfc6
Carlos UchOa <carlosjuchoa@hotmail.com>
2021-03-16 14:57:13 -0300

    tipos de funções js

--- index.js
+++ index.js
@@ -6,10 +6,11 @@
 
 //formas de pegar elemento
 //pegar elemento pela tag
-var txtTagName = document.getElementsByTagName("h1");
-console.log(txtTagName[2].innerHTML);
+// var txtTagName = document.getElementsByTagName("h1");
+// console.log(txtTagName[2].innerHTML);
 
-txtTagName[2].setAttribute("class", "txtBlue");
+// txtTagName[2].setAttribute("class", "txtBlue");
+//aqui o elemento mudou de vermelho para azul
 
 
 
@@ -18,3 +19,26 @@ txtTagName[2].setAttribute("class", "txtBlue");
 //document.getElementsByClassName();
 // var txtClassName = document.getElementsByClassName("txtName");
 // console.log(txtClassName);
+//as funções podem ter os nomes que quiserem, nessa estamos pegando
+// os elementos pelo ID
+function getById(id){
+var txt = document.getElementById(id);
+return txt;
+}
+
+function getByTag(tag){
+var txtTagName = document.getElementsByTagName(tag);
+return txtTagName;    
+}
+
+function getByClass(className){
+var txtClassName = document.getElementsByClassName(className);
+return txtClassName;
+}
+
+//como chamar essas funções de maneira correta e de maneira errada
+
+console.log(getById("primeiroHello"));
+console.log(getByTag("h1")); 
+console.log(getByClass("txtName")); 
+
