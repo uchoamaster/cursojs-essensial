1c4663a073ea48bd4f044c011f6a14c6318c2ba2
Carlos Uchoa <carlosjuchoa@hotmail.com>
2021-03-16 20:04:10 -0300

    aplicando o boostrap no projeto de login

---
+++ codigoBootstrap/validaForm.js
@@ -0,0 +1,18 @@
+function validaEmail(id){
+    //search
+    //length
+
+    //tamanho > 5
+    //deve existir o caracter @
+    var email = document.getElementById(id).value;
+    console.log('email');
+}
+
+function validaSenha(id){
+
+    //search
+    //length
+    //setAttribute("","")
+
+    //deve ter mais do que 5 caracteres
+}
